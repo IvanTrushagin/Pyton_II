13cc1f5ba4c58d0d20f74b19ddf59191cf287528
ivan_trushagin <165751736+IvanTrushagin@users.noreply.github.com>
2024-05-29 09:26:17 +0500

    Ввод-Вывод, операторы ветвления

--- Seminars/Sem1/Task1.py
+++ Seminars/Sem1/Task1.py
@@ -72,9 +72,9 @@
 # Output: YES
 
 
-n = int(input())
-if n % 4 == 0 and n % 100!= 0 or n % 400 == 0:
-    print('Yes')
-else:
-    print('No')
+# n = int(input())
+# if n % 4 == 0 and n % 100!= 0 or n % 400 == 0:
+#     print('Yes')
+# else:
+#     print('No')
 
